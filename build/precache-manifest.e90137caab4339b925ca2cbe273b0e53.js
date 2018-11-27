888765650a4d4f417d295bb23bc3f3de40856fd4
unknown <zbignevus@gmail.com>
2018-11-27 02:58:06 +0200

    final sorted

---
+++ build/precache-manifest.e90137caab4339b925ca2cbe273b0e53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "065ad457431687a859b6",
+    "url": "/placard2/static/js/runtime~main.065ad457.js"
+  },
+  {
+    "revision": "7cc6ec2da91725d50a7e",
+    "url": "/placard2/static/js/main.7cc6ec2d.chunk.js"
+  },
+  {
+    "revision": "ffb28e0a032f494bed98",
+    "url": "/placard2/static/js/1.ffb28e0a.chunk.js"
+  },
+  {
+    "revision": "7cc6ec2da91725d50a7e",
+    "url": "/placard2/static/css/main.dad2be91.chunk.css"
+  },
+  {
+    "revision": "fbf79d65d9bb1d430976ebfddfba9ac7",
+    "url": "/placard2/index.html"
+  }
+];
